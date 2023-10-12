1f467bad363fdb5f5e349596bbacc3b1532dea83
Emery Frank <frankflej@gmail.com>
2023-10-12 09:25:11 +0200

    ft(home-page):Design of the home page
    - Design of the home page
    - Design of the introductory page

---
+++ src/components/about/about.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import Mybutton from '../button/button'
+import Image from 'next/image'
+import { background } from '@/assets'
+import {BsArrowLeft,BsArrowRight} from 'react-icons/bs'
+
+export default function About() {
+  return (
+    <div>
+        <div className='w-[100%] flex align-center justify-end h-[100vh]'>
+           <div className='bg-slate-500 relative h-[85%] w-[65%]'>
+                <div className='absolute left-[-54%] top-[12%] w-[75%] bg-slate-800 h-[80%] bg-cover'
+                 style={{
+                    backgroundImage: `linear-gradient(to bottom,#0101018a,#0101018a), url(${background.src})`,
+                  }}>
+       <div className='flex w-[100%] h-[100%] justify-end items-end'>
+            <div className='flex gap-[0.1rem]'>
+            <div className='flex items-center justify-center bg-slate-300 w-[5rem] h-[5rem]'>
+                <BsArrowLeft className='text-[2rem]'/>
+            </div>
+            <div className='flex items-center justify-center bg-slate-300 w-[5rem] h-[5rem]'>
+                <BsArrowRight className='text-[2rem]'/>
+            </div>
+            </div>
+       </div>
+                </div>
+                <div className='w-[100%] h-[90%] flex  items-center justify-center '>
+                    <div className='w-[50%]'>
+                    <div id='aboutTitle'>
+                <h4>Rooms & Suites</h4>
+                </div>
+                <div id='aboutSubTitle'>
+                <h5>Stylish rooms and suites on Londons South Bank</h5>
+                </div>
+                <div className='' id='aboutDetail'>
+                    <p>
+                    Perfect for all occasions, our range of contemporary rooms,
+                    suites and penthouses offer the perfect base from which to explore London.
+                    Whether you are here for an overnight stay, a city break, or a family
+                    staycation, we have something to suit.
+                    </p>
+                </div>
+                <div>
+                    <Mybutton bDetail='EXPLORE MORE'/>
+                </div>
+                    </div>
+                </div>
+           </div>
+        </div>
+
+        <div className='w-[100%] flex align-center justify-start h-[100vh]'>
+
+           <div className='bg-slate-500 relative h-[85%] w-[65%]'>
+    <div className='absolute right-[-53.5%] top-[12%] w-[75%] bg-slate-800 h-[80%] bg-cover'
+    style={{
+        backgroundImage: `linear-gradient(to bottom,#0101018a,#0101018a), url(${background.src})`,
+      }}>
+       <div className='flex w-[100%] h-[100%] justify-start items-end'>
+            <div className='flex gap-[0.1rem]'>
+            <div className='flex items-center justify-center bg-slate-300 w-[5rem] h-[5rem]'>
+                <BsArrowLeft className='text-[2rem]'/>
+            </div>
+            <div className='flex items-center justify-center bg-slate-300 w-[5rem] h-[5rem]'>
+                <BsArrowRight className='text-[2rem]'/>
+            </div>
+            </div>
+       </div>
+    </div>
+    <div className='w-[100%] h-[90%] flex  items-center justify-center '>
+        <div className='w-[50%]'>
+        <div id='aboutTitle'>
+    <h4>Rooms & Suites</h4>
+    </div>
+    <div id='aboutSubTitle'>
+    <h5>Stylish rooms and suites on Londons South Bank</h5>
+    </div>
+    <div className='' id='aboutDetail'>
+        <p>
+        Perfect for all occasions, our range of contemporary rooms,
+        suites and penthouses offer the perfect base from which to explore London.
+        Whether you are here for an overnight stay, a city break, or a family
+        staycation, we have something to suit.
+        </p>
+    </div>
+    <div>
+        <Mybutton bDetail='EXPLORE MORE'/>
+    </div>
+        </div>
+    </div>
+    
+
+</div>
+        </div>
+    </div>
+  )
+}
