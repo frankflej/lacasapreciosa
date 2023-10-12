b8b9d55d367e61ae93c9318614e66ff8bfbd70cd
Emery Frank <frankflej@gmail.com>
2023-10-12 09:25:11 +0200

    ft(home-page):Design of the home page
    - Design of the home page
    - Design of the introductory page

---
+++ src/components/introduction/introduction.tsx
@@ -0,0 +1,24 @@
+import Mybutton from "../button/button"
+
+interface intro{
+    introTitle:string,
+    introDetails:string,
+    introBtn:string
+}
+export default function Introduction({introTitle,introDetails,introBtn}:intro) {
+  return (
+    <div className="text-center mt-[6rem] flex justify-center w-full ">
+        <div className="w-[70%]">
+        <div className="my-[3rem] text-[2.3rem]">
+            <p>{introTitle}</p>
+        </div>
+        <div className="mb-[3rem]">
+            <p>{introDetails}</p>
+        </div>
+        <div className="mb-[3rem]">
+            <Mybutton bDetail={introBtn}/>
+        </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/utils/introductionCollection.ts
@@ -0,0 +1,7 @@
+export const introColection=[
+    {
+    introTitle:'Welcome to Park Plaza Westminster Bridge London',
+    introDetails:'Conveniently situated on the South Bank of the Thames and overlooking the Houses of Parliament, Park Plaza Westminster Bridge London is an iconic hotel featuring contemporary design, stylish rooms, and an array of restaurants and bars. After a day of exploring the local area, including the London Eye and Westminster, unwind with a massage at Mandara Spa and a dip in our 15-metre swimming pool.',
+    introBtn:'Explore hotel'
+    }
+]
