bd5c10e6799d9ab573ab6aafd4f6f3711cf56620
Emery Frank <108126328+frankflej@users.noreply.github.com>
2023-11-15 11:33:34 +0200

    remove button background

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -9,7 +9,7 @@ interface Btn {
 export default function Mybutton({ bDetail,showIcon }: Btn) {
   return (
     <div> 
-      <button className='relative text-[13.5px] bg-[#11233a] flex items-center  gap-[0.5rem] rounded-[0.2rem] text-[#fff] border-white border-[2.5px] px-[1.5rem] uppercase py-[0.7rem] cursor hover:bg-[#0101019b] duration-[0.5s]'>
+      <button className='relative text-[13.5px] flex items-center  gap-[0.5rem] rounded-[0.2rem] text-[#fff] border-white border-[2.5px] px-[1.5rem] uppercase py-[0.7rem] cursor hover:bg-[#0101019b] duration-[0.5s]'>
         {bDetail} <span>{showIcon?<BsArrowRight className='text-[1.5rem] text-[#4fb4be]'/>:''}</span>
       </button>
     </div>
