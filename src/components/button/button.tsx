790f4043fe687c6232e79c8e4e99a11871498021
Emery Frank <frankflej@gmail.com>
2023-11-29 16:14:40 +0200

    offer

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -10,7 +10,7 @@ interface Btn {
 export default function Mybutton({ bDetail,showIcon,bgColor }: Btn) {
   return (
     <div> 
-      <button className={`relative text-[13.5px] flex items-center ${bgColor?'bg-[#122339] hover:bg-[#1e3957] font-light text-[12px] text-[#eee]':'bg-transparent hover:bg-[#08111c49]'}  gap-[0.5rem] rounded-[0.2rem] text-[#fff] border-white border-[2.5px] px-[1.5rem] uppercase py-[0.7rem] cursor duration-[0.5s]`}>
+      <button className={`relative text-[13.5px] flex items-center ${bgColor?'bg-[#122339] font-light text-[12px] text-[#eee]':'bg-transparent hover:bg-[#4a97f649] border-white border-[2.5px]'}  gap-[0.5rem] rounded-[0.2rem] text-[#fff]  px-[1.5rem] uppercase py-[0.7rem] cursor duration-[0.5s]`}>
         {bDetail} <span>{showIcon?<BsArrowRight className='text-[1.5rem] text-[#4fb4be]'/>:''}</span>
       </button>
     </div>
--- src/components/offer/index.tsx
+++ src/components/offer/index.tsx
@@ -4,10 +4,10 @@ import { Offer } from "./type"
 
 export default function Offer() {
   return (
-    <div className="w-[100%] mb-[20rem] flex justify-center text-txt-color">
+    <div className="w-[100%] mb-[20rem] pb-[25rem] flex justify-center text-txt-color">
        <div className="w-[65%]">
-       <div className="text-center ">
-            Offer
+       <div className="text-center text-[2rem] mb-5">
+            <h2>Offers</h2>
         </div>
         <div className='flex  bg-main-color w-[100%]'>
             {
@@ -19,17 +19,17 @@ export default function Offer() {
                    }}
                    > 
                 </div>
-                <div className='w-[90%] bg-main-color p-7 border absolute top-[14.5rem]'>
-                    <div className='offerTitle text-[19px] mb-[1rem]'>
-                        <h3>{offer.offerTitle}</h3>
+                <div className='w-[90%] bg-main-color p-7 absolute top-[14.5rem]'>
+                    <div className='offerTitle text-[1.6rem] mb-[1rem]'>
+                        <h2>{offer.offerTitle}</h2>
                     </div>
                     <div>
-                        <p className='offerTitle text-[16.5px] mb-[1rem]'>
+                        <p className='offerTitle text-[16.5px] mb-[2rem]'>
                         {offer.offerDescription}
                         </p>
                     </div>
-                    <div className='offerTitle flex justify-center'>
-                        <Mybutton bDetail={offer.offerBtnlink} showIcon={true}/>
+                    <div className='offerTitle flex w-[100%] justify-center'>
+                        <Mybutton bDetail={offer.offerBtnlink} showIcon={true} bgColor={true}/>
                     </div>
                 </div>
             </div>
