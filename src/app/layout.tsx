80092083244d45b99f1e9fc8644dbeb4fe30255e
Emery Frank <frankflej@gmail.com>
2025-03-09 14:15:34 +0200

    Fixing build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Lacasapreciosa',
+  title: 'La Perla Negra',
 }
 
 export default function RootLayout({
