5ff9443e8423e32e2432432a91cf9e3c38757486
Emery Frank <frankflej@gmail.com>
2025-06-08 17:28:56 +0200

    adding dashboard fix

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -10,12 +10,12 @@ const buttons = [
     showDot: false,
     component: href ? (
       <Link href={href}>
-        <button className={`inline-flex py-2  px-2 md:px-6 md:py-3  animate-shimmer items-center justify-center rounded-md bg-[linear-gradient(110deg,rgba(255,255,255,0.1),45%,rgba(255,255,255,0.5),55%,rgba(255,255,255,0.1))] hover:bg-[linear-gradient(110deg,rgba(255,255,255,0.3),45%,rgba(255,255,255,0.7),55%,rgba(255,255,255,0.3))] bg-[length:200%_100%] font-medium text-white transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ${className}`}>
+        <button className={`inline-flex py-2  px-2 md:px-6 md:py-3   items-center justify-center rounded-md bg-[linear-gradient(110deg,rgba(255,255,255,0.1),45%,rgba(255,255,255,0.5),55%,rgba(255,255,255,0.1))] hover:bg-[linear-gradient(110deg,rgba(255,255,255,0.3),45%,rgba(255,255,255,0.7),55%,rgba(255,255,255,0.3))] bg-[length:200%_100%] font-medium text-white transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ${className}`}>
           {buttonDetail}
         </button>
       </Link>
     ) : (
-      <button className={`inline-flex py-2  px-2 md:px-6 md:py-3  animate-shimmer items-center justify-center rounded-md bg-[linear-gradient(110deg,rgba(255,255,255,0.1),45%,rgba(255,255,255,0.5),55%,rgba(255,255,255,0.1))] hover:bg-[linear-gradient(110deg,rgba(255,255,255,0.3),45%,rgba(255,255,255,0.7),55%,rgba(255,255,255,0.3))] bg-[length:200%_100%] font-medium text-white transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ${className}`}>
+      <button className={`inline-flex py-2  px-2 md:px-6 md:py-3   items-center justify-center rounded-md bg-[linear-gradient(110deg,rgba(255,255,255,0.1),45%,rgba(255,255,255,0.5),55%,rgba(255,255,255,0.1))] hover:bg-[linear-gradient(110deg,rgba(255,255,255,0.3),45%,rgba(255,255,255,0.7),55%,rgba(255,255,255,0.3))] bg-[length:200%_100%] font-medium text-white transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ${className}`}>
         {buttonDetail}
       </button>
     ),
